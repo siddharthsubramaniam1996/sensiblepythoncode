627acfe66038633d01a2c8e5cf657c7b13de53d1
Siddharth Subramaniam <siddharthsubramaniam1996@gmail.com>
2019-07-27 20:42:12 +0530

    Add files via upload

---
+++ fromiter2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 27 19:26:41 2019
+
+@author: SIDDHARTH
+"""
+
+list1=['a','b','c','d','e']
+print(list1)
+tup1=(1,2,3,4,5)
+print(tup1)
---
+++ nparrange.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 27 20:10:24 2019
+
+@author: SIDDHARTH
+"""
+
+import numpy as np
+a1=np.arange(7)
+print(a1)
+
+a2=np.arange(0,10,2,dtype=float)
+print(a2)
