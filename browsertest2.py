1ab7b7153e0dfd92e2a7ad3b003bf5dad72b1ded
Siddharth Subramaniam <siddharthsubramaniam1996@gmail.com>
2020-01-26 20:23:33 +0530

    Update browsertest2.py

--- browsertest2.py
+++ browsertest2.py
@@ -3,23 +3,22 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
-# =============================================================================
+
 # #ACCEPTING INPUTS
-# uname=input("ENTER USERNAME: ")
-# passwd=input("ENTER PASSWORD :")
-# pname=input("ENTER PROJECT NAME: ")
-# =============================================================================
+uname=input("ENTER USERNAME: ")
+passwd=input("ENTER PASSWORD :")
+pname=input("ENTER PROJECT NAME: ")
 
 browser = webdriver.Firefox()
 browser.get('https://onex.osourceglobal.com/MZSK/Attendance/frmEmpSelfAttendance.aspx')
 
 #ENTERING THE USER ID
 username=browser.find_element_by_id('Login1_UserName')
-username.send_keys('MUM2444')
+username.send_keys(uname)
 
 #ENTERING THE PASSWORD
 password=browser.find_element_by_id('Login1_Password')
-password.send_keys('$!dDhu96')
+password.send_keys(pass)
 
 
 #CLICKIKING THE LOGIN BUTTON
@@ -46,6 +45,7 @@ browser.minimize_window
 #FILLING IN THE REMARKS ENTRY - NOT WORKING AS OF NOW
 textentry=browser.find_element_by_id('ctl00_ContentPlaceHolder_dtgAttendance_ctl03_TxtRemarks')
 
-textentry=browser.find_element_by_name('ctl00$ContentPlaceHolder$dtgAttendance$ctl03$TxtRemarks').send_keys('Canara Bank VAPT Project')
+#FILLINF IN THE REMARKS- 2nd attempt - not working
+textentry=browser.find_element_by_name('ctl00$ContentPlaceHolder$dtgAttendance$ctl03$TxtRemarks').send_keys(pname)
 textentry.click()
 time.sleep(5)
